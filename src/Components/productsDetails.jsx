84404dad6a7429967a3fb8486dd21fdb9efb95a5
SaidqodirxonUz <SaidqodirxonUz@gmail.com>
2023-09-28 21:41:16 +0500

    fixed ProductDetails

--- src/Components/productsDetails.jsx
+++ src/Components/productsDetails.jsx
@@ -7,70 +7,74 @@ import axios from "axios";
 
 const More = () => {
   const { id } = useParams();
-  const [product, setProducts] = useState([]);
+  const [product, setProduct] = useState(null); // Initialize as null
+
   useEffect(() => {
     fetch(`https://back.kalde.uz/products/${id}`)
       .then((res) => res.json())
-      .then((data) => setProducts(data.data));
-  }, []);
-  console.log(product);
+      .then((data) => setProduct(data.data))
+      .catch((error) => {
+        console.error("Error fetching product:", error);
+      });
+  }, [id]);
+
+  if (!product) {
+    // Loading state
+    return (
+      <>
+        <Header />
+        <div className="container text-center">
+          <p>Loading...</p>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Header />
       <div className="container">
         <div className="row g-3 mb-5">
-          {product.map((p, index) => (
-            <div key={index} className="col-12 h-100 pt-5">
-              <div className="card overflow-hidden" aria-hidden="true">
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-evenly",
-                    flexDirection: "row",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    src={p.data?.img_url}
-                    className="placeholder-card-image d-flex align-items-center justify-content-center card-image-top bg-light"
-                  />
-                  <img
-                    src={p.data?.img1_url}
-                    className="placeholder-card-image d-flex align-items-center justify-content-center card-image-top bg-light"
-                  />
-                </div>
-
-                <div className="card-body">
-                  <h3 className="card-title text-truncate">
-                    Имя : {p.data.ru_product_name}
-                  </h3>
-                  <div className="card-text">
-                    <h5 className="product-description my-3">
-                      {p.data?.ru_desc}
-                    </h5>
-                    {/* <div className="col-6 d-flex justify-content-between">
-                      <h6 className="fs-2">{p.data?.category}</h6>
-                    </div> */}
-                    {/* </div>  */}
-                    {/* <div className="d-flex g-3 row">
-                      <h6 className="text-danger fs-2">
-                        Цена : {product.price}
-                      </h6> */}
-
-                    <div className="col-6"></div>
-                  </div>
-                </div>
+          <div className="col-12 h-100 pt-5">
+            <div className="card overflow-hidden" aria-hidden="true">
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <img
+                  src={product?.img_url}
+                  className="placeholder-card-image d-flex align-items-center justify-content-center card-image-top bg-light"
+                />
+                <img
+                  src={product?.img1_url}
+                  className="placeholder-card-image d-flex align-items-center justify-content-center card-image-top bg-light"
+                />
               </div>
 
-              <Link
-                to={"/"}
-                className="btn btn-primary w-25 d-flex justify-content-center m-auto fs-6 mt-5"
-              >
-                <i className="fa-solid fa-arrow-left d-flex justify-content-center m-1"></i>{" "}
-                назад
-              </Link>
+              <div className="card-body">
+                <h3 className="card-title text-truncate">
+                  Имя : {product.ru_product_name}
+                </h3>
+                <div className="card-text">
+                  <h5 className="product-description my-3">
+                    {product.ru_desc == "undefined" ? "" : product?.ru_desc}
+                  </h5>
+                </div>
+              </div>
             </div>
-          ))}
+
+            <Link
+              to={"/"}
+              className="btn btn-primary w-25 d-flex justify-content-center m-auto fs-6 mt-5"
+            >
+              <i className="fa-solid fa-arrow-left d-flex justify-content-center m-1"></i>{" "}
+              назад
+            </Link>
+          </div>
         </div>
       </div>
       <Footer />
